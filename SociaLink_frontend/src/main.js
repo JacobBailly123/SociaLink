0e34e717171aedb75e8d565c1b43e6765e2b70ca
Jacob Bailly <jacobbailly2002@gmail.com>
2026-03-20 03:01:19 -0400

    Fix main.js remove duplicate and backticks

--- SociaLink_frontend/src/main.js
+++ SociaLink_frontend/src/main.js
@@ -1,15 +1,11 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
-```
-
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
 
 const app = createApp(App)
 
